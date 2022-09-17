0e4b2404a39cfc7a1c2574098fbc69fbc2f1f545
Nahuel Santillan <84944031+nawesan12@users.noreply.github.com>
2022-09-17 00:32:13 -0300

    a

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const path = require("path")
 const socket = require("socket.io")
+const fs = require("fs")
 
 const app = express()
 
@@ -15,9 +16,24 @@ const server = app.listen(PORT, () => {
 const io = socket(server)
 
 io.on("connection", (socket) => {
-    console.log(`New user connected in the socket: ${socket.id}`)
+    console.log(`New user connected in the socket: ${socket.id}`)      
+
+    const momento = new Date().toLocaleString()
+    
+    if(fs.existsSync("chat.txt")) {
+        console.log("Historial de chat encontrado!")
+    } else {
+        fs.writeFile("chat.txt", "", () => {
+            console.log("Historial creado...")
+        })
+    }
 
     socket.on("chat:message", function(data) {
+        const mensajeFormateado = data.message[0].toUpperCase().concat(data.message.slice(1))
+
+        fs.appendFile("chat.txt", `${data.username.toUpperCase()} - ${momento} \n \t ${mensajeFormateado} \n`, (err) => {
+            console.log("Mensaje añadido!")
+        })
         io.sockets.emit("chat:message", data)
     })
 
